514fca76751e8545511c1c0b9d52a18002638773
Luka Mijović <lukamij@telekom.rs>
2024-09-13 10:44:28 +0200

    Added hashed passwords

--- services/adminService.go
+++ services/adminService.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/LukaMijovic/role-mgmt-access-ctrl/database/repository"
 	"github.com/LukaMijovic/role-mgmt-access-ctrl/model/dto"
+	"github.com/LukaMijovic/role-mgmt-access-ctrl/util"
 )
 
 func SignInAdmin(a *dto.AdminCredentialsDTO) (*dto.UserCredentialsDTO, error) {
@@ -28,7 +29,7 @@ func SignInAdmin(a *dto.AdminCredentialsDTO) (*dto.UserCredentialsDTO, error) {
 		return nil, err
 	}
 
-	if a.Email == creds.Email && a.Password == creds.Password {
+	if a.Email == creds.Email && util.CheckPassword(u.Password, creds.Password) {
 		return creds, nil
 	}
 
--- services/userService.go
+++ services/userService.go
@@ -6,6 +6,7 @@ import (
 	"github.com/LukaMijovic/role-mgmt-access-ctrl/database/repository"
 	"github.com/LukaMijovic/role-mgmt-access-ctrl/model"
 	"github.com/LukaMijovic/role-mgmt-access-ctrl/model/dto"
+	"github.com/LukaMijovic/role-mgmt-access-ctrl/util"
 )
 
 func SignInUser(u *dto.UserCredentialsDTO) (*dto.UserCredentialsDTO, error) {
@@ -23,7 +24,7 @@ func SignInUser(u *dto.UserCredentialsDTO) (*dto.UserCredentialsDTO, error) {
 		return nil, err
 	}
 
-	if u.Email == creds.Email && u.Password == creds.Password {
+	if u.Email == creds.Email && util.CheckPassword(u.Password, creds.Password) {
 		return creds, nil
 	}
 
@@ -38,6 +39,14 @@ func RegisterUserToDatabase(u *dto.UserCredentialsDTO) error {
 	if err != nil {
 		return err
 	}
+
+	hashedPass, err := util.HashPassword(u.Password)
+
+	if err != nil {
+		return err
+	}
+
+	u.Password = hashedPass
 
 	err = userRepository.SaveUserCredentials(u)
 
